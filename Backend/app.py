a8af5d98f93ac1bf3aba2614601229ed4af3d582
Yaninna Alvarez <134446812+Yaninna137@users.noreply.github.com>
2025-06-30 20:19:22 -0400

    API en History_product
    Se incluyo api para purchase_history

--- Backend/app.py
+++ Backend/app.py
@@ -4,9 +4,10 @@ Punto de entrada de la API Flask
 from db import app
 from routes.clientes import clientes_bp
 from routes.productos import productos_bp
+from routes.historial import history_bp
 
 app.register_blueprint(clientes_bp)
 app.register_blueprint(productos_bp)
-
+app.register_blueprint(history_bp)
 if __name__ == '__main__':
     app.run(debug=True)
--- Backend/models.py
+++ Backend/models.py
@@ -4,6 +4,7 @@ Definiciones de tablas SQLAlchemy
 from db import db
 from sqlalchemy.dialects.postgresql import UUID
 import uuid
+from datetime import datetime
 
 class Client(db.Model):
     __tablename__ = 'client'
@@ -21,3 +22,13 @@ class Product(db.Model):
     price = db.Column(db.Numeric, nullable=False)
     image = db.Column(db.String)        
     description = db.Column(db.String)   
+
+class PurchaseHistory(db.Model):
+    __tablename__ = 'purchase_history'
+    id_history = db.Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    id_user = db.Column(UUID(as_uuid=True), db.ForeignKey('client.id_user'), nullable=False)
+    id_product = db.Column(UUID(as_uuid=True), db.ForeignKey('product.id_product'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False)
+    date_time = db.Column(db.DateTime, default=datetime.utcnow)
+    client = db.relationship('Client', backref='purchases')   # FK de client
+    product = db.relationship('Product', backref='purchases') # FK de product
